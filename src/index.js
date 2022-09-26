8ee8d93fd28a370f7c8644a091f1042293759bbb
Mohammed Dawood <mahmedtony73@gmail.com>
2022-09-26 20:28:43 +0200

    Alternative: Using the Context API

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { combineReducers, createStore } from "redux";
+// import { Provider } from "react-redux";
+// import { combineReducers, createStore } from "redux";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
-import productReducer from "./store/reducers/products";
+import ProductsProvider from "./context/product-context";
+// import productReducer from "./store/reducers/products";
 
-const rootReducer = combineReducers({
-  shop: productReducer,
-});
+// const rootReducer = combineReducers({
+//   shop: productReducer,
+// });
 
-const store = createStore(rootReducer);
+// const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <Provider store={store}>
+  <ProductsProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
+  </ProductsProvider>
 );
 
 // import React from "react";
